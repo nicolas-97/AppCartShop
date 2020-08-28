001c8c7692f9d51d24034588f30f350f6eaa9f19
Nicolas <nicolas.sabogalto@utadeo.edu.co>
2020-08-28 00:47:22 -0500

    api services

--- src/app/shared/services/product-cart.service.ts
+++ src/app/shared/services/product-cart.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductCartService {
 
-  constructor() { }
+  apiUrl:String="http://127.0.0.1:8000/api/cartShop/"
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+  constructor(private http:HttpClient) { }
+
+  get(id):Observable<any>{
+    return this.http.get(`${this.apiUrl+id}`);
+  }
+
+  post(dataProduct){
+    return this.http.post(`${this.apiUrl}`,dataProduct)
+  }
+
+  put(dataProduct){
+    return this.http.put(`${this.apiUrl}`,dataProduct)
+  }
+
+  delete(idProduct){
+    return this.http.delete(`${this.apiUrl+idProduct}`)
+  }
 }
